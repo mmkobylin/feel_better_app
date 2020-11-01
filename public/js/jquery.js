d4eca67626ca9447b8c6f9362fdd68f92ec12c73
mmkobylin <mmkobylin@gmail.com>
2020-11-01 14:42:49 +0000

    element selected via id

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -1,1 +1,7 @@
-console.log('test')
+// making sure the content is first loaded
+document.addEventListener('DOMContentLoaded', () => {
+
+    const show = document.getElementById('subtitle1')
+
+    console.log(show);
+});
