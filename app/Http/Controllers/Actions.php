66262f9a2861acfef33a2ce07a612cef6675c886
mmkobylin <mmkobylin@gmail.com>
2020-11-01 13:35:11 +0000

    action table populated and displaying

--- app/Http/Controllers/Actions.php
+++ app/Http/Controllers/Actions.php
@@ -6,11 +6,5 @@ use Illuminate\Http\Request;
 
 class Actions extends Controller
 {
-    public function index()
-    {
-        return view ("home", [
-            "actions" => Action::all(),
-        ]);
-            
-    }
+
 }
