d97d9944e6a6286dbc3e765152430592171ec9ad
mmkobylin <mmkobylin@gmail.com>
2020-11-01 14:53:56 +0000

    eventlistener added to show paragrph

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -1,7 +1,19 @@
 // making sure the content is first loaded
 document.addEventListener('DOMContentLoaded', () => {
+    
 
-    const show = document.getElementById('subtitle1')
+    const subtitle1 = document.getElementById('subtitle1')
 
-    console.log(show);
+    const paragraph1 = document.getElementById('paragraph1')
+
+    let visible = false
+
+    subtitle1.addEventListener('click', () => {
+        
+        paragraph1.style.display = visible? 'block' : 'none'
+
+    })
+
+    visible = !visible
+
 });
--- resources/views/partials/intro.blade.php
+++ resources/views/partials/intro.blade.php
@@ -10,7 +10,7 @@
             </p>
         
             <h3 id="subtitle1" style="display: block">Tell me more!</h3>
-            <p id='extend-paragraph1' style="display: none">
+            <p id='paragraph1' style="display: none">
             Let's say you have been in a dark place. Might be for a week. Might be for a month, or you really don't know for how long.
             But it has been attrocious. You have eaten all takeaway. Or opposite, nearly nothing at all. You don't remember when you washed your hair. The house is a mess. You still are not exactly energetic, but the reality had come knocking on the door. <br>
             First of all, I feel you. This is what the <a class="navbar" href="/lightbringer">LIGHTBRINGER</a>. is design for! I wanted to create an app where by simple and cheap or free steps you are able to feel just a little bit better. Maybe not every day, but often enough.</p>
@@ -24,7 +24,7 @@
             </p>
 
             <h3 id="subtitle2" style="display: block">Tell me more!</h3>
-            <p id="extend-paragraph2" style="display: none">
+            <p id="paragraph2" style="display: none">
                 Maybe you just just got most of your ducks in order. Maybe it is the other way around, you used to and now they seem to be having a rave. Maybe you just need something to keep you in the routine. <br>
                 This is what I created a <a class="navbar" href="/heavylifter">HEAVYLIFTER</a> for. <br>
                 I created a list of checking boxes, that you might tick, and get the score you had after each day. 
