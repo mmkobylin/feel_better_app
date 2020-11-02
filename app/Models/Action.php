32c9407e53c037c396bd343d9fce58011b81ccec
mmkobylin <mmkobylin@gmail.com>
2020-11-02 17:47:37 +0000

    randomiser modified so actions do not repeat themselves

--- app/Models/Action.php
+++ app/Models/Action.php
@@ -20,21 +20,17 @@ class Action extends Model
     }
 
     public function randomiser()
-    {
+    {   
         $action1 = Action::all()->random(1)->pluck('action')->toArray()[0];
-        $action2 = Action::all()->random(1)->pluck('action')->toArray()[0];
-        $action3 = Action::all()->random(1)->pluck('action')->toArray()[0];
-
-        if ( $action1 === $action2) {
+        if (!isset($action2) || $action1 === $action2) {
             $action2 = Action::all()->random(1)->pluck('action')->toArray()[0];
-        } elseif ($action1 === $action3) 
-        {
+        } 
+        
+        if (!isset($action3) || $action1 === $action3 || $action2 === $action3) {
             $action3 = Action::all()->random(1)->pluck('action')->toArray()[0];
         }
 
-        $arrayOfActions = [$action1, $action2, $action3];
-
-        echo implode($arrayOfActions);
+        return $action1 . $action2 . $action3;
 
     }
 }
