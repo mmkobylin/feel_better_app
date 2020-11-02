5a0d78fbbfbeac5a6b089d6945de02a05571d406
mmkobylin <mmkobylin@gmail.com>
2020-11-02 10:38:12 +0000

    new intro paragraph applied

--- resources/views/partials/original-heavylifter.blade.php
+++ resources/views/partials/original-heavylifter.blade.php
@@ -5,11 +5,14 @@
     </h3>
     <p id="paragraph2" style="display: none">
         Maybe you just just got most of your ducks in order. Maybe it is the other way around, you used to and now they seem to be having a rave. Maybe you just need something to keep you in the routine. <br>
-        This is what I created a <a class="navbar" href="/heavylifter">HEAVYLIFTER</a> for. <br>
+        This is what I created a <a class="navbar-brand" href="/heavylifter">HEAVYLIFTER</a> for. <br><br>
         I created a list of checking boxes, that you might tick, and get the score you had after each day. 
         It is ok if some of them don't apply. This is your journey. 
-        If you prefer to <br><a class="nav-home" href="/heavy-custom">Pick your own adventure</a><br>
-        Just click on link and put some of your own ideas of things that will make you feel better!
+        <br><br>
+        But what if you are thinking... nah, this sounds good, but I know I feel good only when I sleep 4 hours and do all the food prep in the world! This does not seem to match my needs at all! <br>
+        Well worry not, with thought of you and adventures spirits, I had created a questionare:
+        <br><a class="nav-home" id="hideaway" href="/heavy-custom">Pick your own adventure?!</a>
+        <br>Please use it and have fun, who am I to tell you how to live your life?
     </p>
 </section>
 
