0451d8c6db0e449b50b26f490dd5f04453b7ca96
mmkobylin <mmkobylin@gmail.com>
2020-11-05 13:32:07 +0000

    routing to lightbringer and basic heavylifter works

---
+++ app/Http/Controllers/Heavylifters.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Heavylifter; 
+
+//set up methods via controller
+class Heavylifters extends Controller
+{
+
+    public function show() {
+        return view('home')
+    }
+
+    public function index()
+    {
+        return Heavylifter::all();           
+    }
+
+    public function show_basic() {
+        return view('heavylifter-basic')
+    }
+
+    public function create(){
+        return view("form");
+    }
+
+    public function createPost($request)
+    {
+        $data = $request->all();
+        $heavylifter = Heavylifter::create($data);
+    }
+}
