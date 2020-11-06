1e2ea281aad7959e91491f81505a85965b70c811
mmkobylin <mmkobylin@gmail.com>
2020-11-06 13:41:48 +0000

    info and links added; issue with routes prevail

---
+++ resources/views/partials/links.blade.php
@@ -0,0 +1,14 @@
+<h3> Links section: </h3>
+
+<h4> Other websites that make you feel better: <h4>
+<p>Tired of cats videos? Or maybe just fancing some variety? </p>
+<a href="https://www.zooborns.com/?cnn=yes"><span>Zoobrons</span></a>
+
+
+<p>Life fell apart? This guy knows this is exactly what is it like: </p>
+<a href="http://1000awesomethings.com/"><span>1000 awesome things</span></a>
+
+
+<p> Wanting more science? Here there are couple of webpages of happiness and wellbeing geeks: </p>
+
+<a href="https://gretchenrubin.com//"><span>Gretchen Rubin website</span></a>
