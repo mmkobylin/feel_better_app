20d6bf05c0d5ea2cf943a65b6f5a5d98d1a12288
mmkobylin <mmkobylin@gmail.com>
2020-11-10 15:37:01 +0000

    home link and view fixed

--- resources/views/partials/links.blade.php
+++ resources/views/partials/links.blade.php
@@ -1,14 +1,14 @@
 <section>
     <h4> Other websites that make you feel better: <h4>
     <p>Tired of cats videos? Or maybe just fancing some variety? </p>
-    <a href="https://www.zooborns.com/?cnn=yes"><span>Zoobrons</span></a>
+    <a href="https://www.zooborns.com/?cnn=yes" class="link"><span>Zoobrons</span></a>
 
 
     <p>Life fell apart? This guy knows this is exactly what is it like: </p>
-    <a href="http://1000awesomethings.com/"><span>1000 awesome things</span></a>
+    <a href="http://1000awesomethings.com/" class="link"><span>1000 awesome things</span></a>
 
 
     <p> Wanting more science? Here there are couple of webpages of happiness and wellbeing geeks: </p>
 
-    <a href="https://gretchenrubin.com//"><span>Gretchen Rubin website</span></a>
+    <a href="https://gretchenrubin.com//" class="link"><span>Gretchen Rubin website</span></a>
 </section>
--- resources/views/partials/title.blade.php
+++ resources/views/partials/title.blade.php
@@ -1,1 +1,1 @@
-<h1>Feel Better!<h1>
+<h1>Feel Better!</h1>
