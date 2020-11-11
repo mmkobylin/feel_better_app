0c537004959dc9ac8a467b418a255ba5ab58a973
mmkobylin <mmkobylin@gmail.com>
2020-11-11 14:29:11 +0000

    events added to info view

--- resources/views/partials/info.blade.php
+++ resources/views/partials/info.blade.php
@@ -1,24 +1,33 @@
+    <h2>Further Help</h2>
+    
+    <article>
+        
+        <h4> What this will not do?</h4>
 
+        <p>
+            This is not an substitute for general health professional. If you need one, please go <a href>HERE</a> and find resources that are right for you.
+        </p>
+
+    </article>
     <section>
-        <br>
-        <h3>Common sense</h3> 
-        <p>This one is more applicable to the <a class="link" href="/lightbringer">LIGHTBRINGER</a>. <br>
+        
+        <h4 id="subtitle1"> 
+        <span id=span1 style="display: block">Common Sense </span>
+        <span id=span2 style="display: none">Got it, thanks! </span>
+        </h4>
+        <p id='paragraph1' style="display: none">
+            This one is more applicable to the <a class="link" href="/lightbringer">LIGHTBRINGER</a>. <br>
             Remember, this is not your personal wheel of torture. Use your own guidance and common sense. You got 'go for the walk' but you are living in Alberta and it is - 40 degrees? Probably not thev best idea, try again. You got 'play happy music' but your spouse and newly born just fell asleep after atrocious day? Use headphones.  
             Same goes for issues with self-esteem, anxiety, eating disorders and such. BUT. Unless it would be stupid or dangerous, please at least try to fullfill the dot - more dots mean you are on the way to feel better, even when you don't feel that way. 
-        </p>
+            <br>
 
-        <p>Overall, I want you to know that THIS IS A PROCESS. And you will never get it done, it will never be perfect, and that's ok.<br>
+            Overall, I want you to know that THIS IS A PROCESS. And you will never get it done, it will never be perfect, and that's ok.<br>
             We named it 'Feel better' rather than 'Be perfect' . My genuine wish for you is to enjoy this journey, and go as far as you want, at your own pace. 
-        </p>
-    </section>
-
-    <section>
-        <h3>What will this not do?</h3> 
-        <p> This is not an substitute for general health professional. If you need one, please go <a class="link" href>HERE</a> and find resources that are right for you.
+            <br>
             I know this is not easy. I am proud of you for looking for help. <br>
             Another thing that this is not, it is not a professional trainer or eating program. I am fat positive and trans-inclusive, and I believe the health and happiness can be find in any size, and if anyone tells you otherwise, it is because they are unhappy with themselves. PROMISE. 
             That's why all the dots are called: 'eat HEALTHIER' not ' healthy'. If you eat only salad all day and be miserable about it, this is not the outcome that you want, right? You finally got to eat home made meal instead of takeout? Great! Made something with vegetables you don't like? Fabolous! Eaten five Oreos instead of entire pack like yesterday? Congratulations, here is your dot!
             
         </p>
-
     </section>
+
