86aee620fb24f85572b5e5bdf021ee036ae783ec
mmkobylin <mmkobylin@gmail.com>
2020-11-11 09:18:27 +0000

    cursor pointer added to links

--- resources/views/partials/original-heavylifter.blade.php
+++ resources/views/partials/original-heavylifter.blade.php
@@ -1,4 +1,84 @@
 <section>
+    {{-- creating the form --}}
+    <form class="form-control" method="POST" action="heavylifter">
+    
+        <h3>Did you...<h3>
+
+        {{-- I am using fieldset for group of related elements in the form  --}}
+        <fieldset class="card-body">
+        {{-- Cross-Site Request Forgery Token --}}
+            @csrf
+            <div class='column'>
+                <label class="container">done some self care?
+                    <input type="checkbox" name="action1" value="1"/>
+                    <span class="checkmark"></span>
+                </label>
+
+                <label class="container">helped others?
+                    <input type="checkbox" name="action2" value="1"/>
+                    <span class="checkmark"></span>
+                </label> 
+            
+                <label class="container">eaten healthier?
+                    <input type="checkbox" name="action3" value="1"/>
+                    <span class="checkmark"></span>
+                </label>
+            </div>
+
+            <div class='column'>
+
+                <label class="container">had a decent sleep?
+                    <input type="checkbox" name="action4" value="1"/>
+                    <span class="checkmark"></span>
+                </label>
+                <label class="container">stayed sober?
+                    <input type="checkbox" name="action5" value="1"/>
+                    <span class="checkmark"></span>
+                </label>
+                <label class="container">exercised?
+                    <input type="checkbox" name="action6" value="1"/>
+                    <span class="checkmark"></span>
+                </label>
+            </div>
+
+            <button class="button" id="subtitle1" style="display: block" 
+            type="submit" name="formSubmit" value="Submit">Submit!</button>
+        </fieldset>
+    </form>
+
+    <?php
+        //collecting all the points
+        $total = (
+            +(isset($_POST["action1"]) ? ($_POST["action1"]): 0) + 
+            +(isset($_POST["action2"]) ? ($_POST["action2"]): 0) +
+            +(isset($_POST["action3"]) ? ($_POST["action3"]): 0) +
+            +(isset($_POST["action4"]) ? ($_POST["action4"]): 0) +
+            +(isset($_POST["action5"]) ? ($_POST["action5"]): 0) +
+            +(isset($_POST["action6"]) ? ($_POST["action6"]): 0)
+        );         
+    ?>
+    {{-- hide if it form is not posted --}}
+    <div <?php if (isset($_POST["formSubmit"]) === false ){ ?> hidden <?php } ?>>
+    {{-- Here go the results --}}
+        <?php
+            echo "The total amount of points for the day is... " . $total . "!"; 
+            {{-- isset($_POST["formSubmit"]) ? print ($_POST["action1"]) + ($_POST["action2"]) + ($_POST["action3"]) + ($_POST["action4"]) + ($_POST["action5"]) + ($_POST["action6"])) : NULL;  --}}
+            
+            ?> <br> <?php
+            //-- comment showing with results --}}
+            if ($total === 0) {
+                echo "It's ok! Bet today was tough! Tomorrow is another day. In a meantime, be nice to yourself.";
+            } elseif ( $total > 0 && $total < 2 ) {
+                echo "Well done, every little step counts!";
+            } elseif ($total >= 2 && $total < 4 ) {
+                echo "That's an excellent progress. Keep going!";
+            } elseif ($total > 4 ) {
+                echo "Amazing job! Look at you go!";
+            }
+        ?> 
+    </div>
+</section>
+<section>
     <h3 id="subtitle2"> 
         <span id=span3 style="display: block">Tell me more! </span>
         <span id=span4 style="display: none">This is how is works! </span>
@@ -16,84 +96,3 @@
         </p>
     </div>
 </section>
-
-<section>
-    {{-- creating the form --}}
-    <form class="form-control" method="POST" action="heavylifter">
-    
-        <h3>Did you...<h3>
-
-        {{-- I am using fieldset for group of related elements in the form  --}}
-            <fieldset class="card-body">
-            {{-- Cross-Site Request Forgery Token --}}
-                @csrf
-                    <div class='column'>
-                        <label class="container">done some self care?
-                            <input type="checkbox" name="action1" value="1"/>
-                            <span class="checkmark"></span>
-                        </label>
-
-                        <label class="container">helped others?
-                            <input type="checkbox" name="action2" value="1"/>
-                            <span class="checkmark"></span>
-                        </label> 
-                    
-                        <label class="container">eaten healthier?
-                            <input type="checkbox" name="action3" value="1"/>
-                            <span class="checkmark"></span>
-                        </label>
-                    </div>
-
-                    <div class='column'>
-
-                        <label class="container">gave yourself fair chance to have decent sleep?
-                            <input type="checkbox" name="action4" value="1"/>
-                            <span class="checkmark"></span>
-                        </label>
-                        <label class="container">stayed sober?
-                            <input type="checkbox" name="action5" value="1"/>
-                            <span class="checkmark"></span>
-                        </label>
-                        <label class="container">exercised?
-                            <input type="checkbox" name="action6" value="1"/>
-                            <span class="checkmark"></span>
-                        </label>
-                    </div>
-
-                    <button class="button" id="subtitle1" style="display: block" 
-                    type="submit" name="formSubmit" value="Submit">Submit!</button>
-                </fieldset>
-            </form>
-
-             <?php
-                //collecting all the points
-                $total = (
-                    +(isset($_POST["action1"]) ? ($_POST["action1"]): 0) + 
-                    +(isset($_POST["action2"]) ? ($_POST["action2"]): 0) +
-                    +(isset($_POST["action3"]) ? ($_POST["action3"]): 0) +
-                    +(isset($_POST["action4"]) ? ($_POST["action4"]): 0) +
-                    +(isset($_POST["action5"]) ? ($_POST["action5"]): 0) +
-                    +(isset($_POST["action6"]) ? ($_POST["action6"]): 0)
-                );         
-            ?>
-            {{-- hide if it form is not posted --}}
-            <div <?php if (isset($_POST["formSubmit"]) === false ){ ?> hidden <?php } ?>>
-            {{-- Here go the results --}}
-                <?php
-                    echo "The total amount of points for the day is... " . $total . "!"; 
-                    {{-- isset($_POST["formSubmit"]) ? print ($_POST["action1"]) + ($_POST["action2"]) + ($_POST["action3"]) + ($_POST["action4"]) + ($_POST["action5"]) + ($_POST["action6"])) : NULL;  --}}
-                    
-                    ?> <br> <?php
-                    //-- comment showing with results --}}
-                    if ($total === 0) {
-                        echo "It's ok! Bet today was tough! Tomorrow is another day. In a meantime, be nice to yourself.";
-                    } elseif ( $total > 0 && $total < 2 ) {
-                        echo "Well done, every little step counts!";
-                    } elseif ($total >= 2 && $total < 4 ) {
-                        echo "That's an excellent progress. Keep going!";
-                    } elseif ($total > 4 ) {
-                        echo "Amazing job! Look at you go!";
-                    }
-                ?> 
-            </div>
-        </section>
