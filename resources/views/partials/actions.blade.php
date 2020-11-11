6679fb82841c41394051ec533e920dcae581cc83
mmkobylin <mmkobylin@gmail.com>
2020-11-11 09:03:34 +0000

    message fixed in lightbringer

--- resources/views/partials/actions.blade.php
+++ resources/views/partials/actions.blade.php
@@ -1,17 +1,5 @@
-<section>
-    <h3 id="subtitle1"> 
-        <span id=span1 style="display: block">Tell me more! </span>
-        <span id=span2 style="display: none">This is how it works! </span>
-    </h3>
-    <p id='paragraph1' style="display: none">
-    Let's say you have been in a dark place. Might be for a week. Might be for a month, or you really don't know for how long.
-    But it has been attrocious. You have eaten all takeaway. Or opposite, nearly nothing at all. You don't remember when you washed your hair. The house is a mess. You still are not exactly energetic, but the reality had come knocking on the door. <br>
-    First of all, I feel you. This is what the LIGHTBRINGER is design for! I wanted to create an app where by simple and cheap or free steps you are able to feel just a little bit better. Maybe not every day, but often enough.</p>
-    <p>If you click 'Show suggestions' it will show couple feel-good ideas. When you are done, just tick the boxes and submit the form!</p>
-</section>
-
 <h3 id="subtitle2" > 
-    <span id=span3 style="display: block">Show me suggestions! </span>
+    <span id=span3 style="display: block">Show suggestions! </span>
     <span id=span4 style="display: none">Try some of these!</span>
 </h3>
 
@@ -74,12 +62,17 @@
         {{-- different message displaying depending on the total amount --}}
 
         <?php
-            if($total > 0 && $total < 2 ) {
-                echo "Well done, every little step counts!";
-            } elseif ($total = 2 && $total < 4 ) {
-                echo "That's an excellent progress. Keep going!";
-            } elseif ($total > 2 ) {
-                echo "Amazing job! Look at you go!";
+            if ($total === 0) {
+            echo "It's ok! Bet today was tough! Tomorrow is another day. In a meantime, be nice to yourself.";
+            } else {
+
+                if ($total > 0 && $total < 2 ) {
+                    echo "Well done, every little step counts!";
+                } elseif ($total = 2 && $total < 4 ) {
+                    echo "That's an excellent progress. Keep going!";
+                } elseif ($total > 2 ) {
+                    echo "Amazing job! Look at you go!";
+                }
             }
         ?>
     </div>
@@ -93,3 +86,14 @@
     @endphp
 </aside>
 
+<section>
+    <h3 id="subtitle1"> 
+        <span id=span1 style="display: block">Tell me more! </span>
+        <span id=span2 style="display: none">This is how it works! </span>
+    </h3>
+    <p id='paragraph1' style="display: none">
+    Let's say you have been in a dark place. Might be for a week. Might be for a month, or you really don't know for how long.
+    But it has been attrocious. You have eaten all takeaway. Or opposite, nearly nothing at all. You don't remember when you washed your hair. The house is a mess. You still are not exactly energetic, but the reality had come knocking on the door. <br>
+    First of all, I feel you. This is what the LIGHTBRINGER is design for! I wanted to create an app where by simple and cheap or free steps you are able to feel just a little bit better. Maybe not every day, but often enough.</p>
+    <p>If you click 'Show suggestions' it will show couple feel-good ideas. When you are done, just tick the boxes and submit the form!</p>
+</section>
