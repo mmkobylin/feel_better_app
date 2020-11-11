291e6c21ce69c034a99a552daad0ff51012912dc
mmkobylin <mmkobylin@gmail.com>
2020-11-11 12:42:59 +0000

    titles and paragraphs sorted for mobile screen

--- resources/views/partials/intro.blade.php
+++ resources/views/partials/intro.blade.php
@@ -2,7 +2,7 @@
     <h2>How does it help?</h2>
 
     <section>
-        <h4>My first goal with this app is to try my best to help anyone in a dark place. (cause I've been there). <br>
+        <h4>My first goal with this app is to try my best to help anyone in a dark place, cause I've been there myself. <br>
             For that go to <a class="nav link" href="/lightbringer">LIGHTBRINGER</a>
         </h4>
 
@@ -13,7 +13,7 @@
         <p id='paragraph1' style="display: none">
         Let's say you have been in a dark place. Might be for a week. Might be for a month, or you really don't know for how long.
         But it has been attrocious. You have eaten all takeaway. Or opposite, nearly nothing at all. You don't remember when you washed your hair. The house is a mess. You still are not exactly energetic, but the reality had come knocking on the door. <br>
-        First of all, I feel you. This is what the <a class="link" href="/lightbringer">LIGHTBRINGER</a> is design for! I wanted to create an app where by simple and cheap or free steps you are able to feel just a little bit better. Maybe not every day, but often enough.</p>
+        First of all, I feel you. This is what the <a class="link" href="/lightbringer">LIGHTBRINGER</a> is design for! I wanted to create an app where by simple and cheap or free steps you are able to feel just a little bit better. Maybe not every day, but often enough.
         </p>
     </section>
 
