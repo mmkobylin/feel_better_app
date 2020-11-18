191d7da87d846a8f423a2b2cbfd824910c6c3759
mmkobylin <mmkobylin@gmail.com>
2020-11-18 14:51:01 +0000

    new paragraph updated for heavylifter

--- resources/views/partials/original-heavylifter.blade.php
+++ resources/views/partials/original-heavylifter.blade.php
@@ -82,20 +82,18 @@
 </section>
 
 <section>
-    <h3 id="subtitle2"> 
+    <h4 id="subtitle2"> 
         <span id=span3 style="display: block">Tell me more! </span>
-        <span id=span4 style="display: none">This is how is works! </span>
-    </h3>
-    <div id="paragraph2" style="display: none"> 
-        <p> Maybe you just just got most of your ducks in order. Maybe it is the other way around, you used to and now they seem to be having a rave. Maybe you just need something to keep you in the routine. <br>
-            This is what I created a heavylifter for.
-            I created a list of checking boxes, that you might tick, and get the score you had after each day. 
-            It is ok if some of them don't apply. This is your journey.</p>
-            <h4> Not for you? </h4>
-            <p> But what if you are thinking... nah, this sounds good, but I know I feel good only when I sleep 4 hours and do all the food prep in the world! This does not seem to match my needs at all! <br>
-            Well worry not, with thought of you and adventures spirits, I had created a questionare:
-            <br><a class="nav link" href="/form">Pick your own adventure?!</a>
-            <br>Please use it and have fun, who am I to tell you how to live your life?
-        </p>
-    </div>
+        <span id=span4 style="display: none">Got it, thanks! </span>
+    </h4>
+
+    <p id="paragraph2" style="display: none">
+        Maybe you just just got most of your ducks in order. Maybe it is the other way around, you used to and now they seem to be having a rave. Maybe you just need something to keep you in the routine. <br>
+        This is what I created a <a class="link" href="/heavylifter">HEAVYLIFTER</a> for. <br>
+        I created a items to be checked off on a list every day, like part of a healthier daily routine.
+        You get the score you had after each day. 
+        It is ok if some of items don't apply. This is your journey. 
+        If you prefer to <br><a class="nav-home link" href="/form">pick your own adventure</a><br>
+        Just click on link and put some of your own ideas of things that will make you feel better!
+    </p>
 </section>
