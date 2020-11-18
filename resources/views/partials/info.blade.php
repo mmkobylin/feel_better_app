385d7a00be5e08b4a3cc772e3c27dcb9425d170f
mmkobylin <mmkobylin@gmail.com>
2020-11-18 13:44:55 +0000

    common sense section shorten

--- resources/views/partials/info.blade.php
+++ resources/views/partials/info.blade.php
@@ -15,17 +15,17 @@
     </h4>
     
     <p id='paragraph1' style="display: none">
-        This one is more applicable to the <a class="link" href="/lightbringer">LIGHTBRINGER</a>. <br>
-        Remember, this is not your personal wheel of torture. Use your own guidance and common sense. You got 'go for the walk' but you are living in Alberta and it is - 40 degrees? Probably not thev best idea, try again. You got 'play happy music' but your spouse and newly born just fell asleep after atrocious day? Use headphones.  
-        Same goes for issues with self-esteem, anxiety, eating disorders and such. BUT. Unless it would be stupid or dangerous, please at least try to fullfill the dot - more dots mean you are on the way to feel better, even when you don't feel that way. 
+        This one is more applicable to the <a class="link" href="/lightbringer">LIGHTBRINGER</a>. <br><br> 
+        Use your own guidance and common sense. You got 'go for the walk' but you are living in Alberta and it is - 40 degrees? Probably not the best
+        idea, try again. Same goes for issues with self-esteem, anxiety, eating disorders and such. BUT. Unless it would be stupid or dangerous, please at least try to fullfill the dot. More dots mean you are on the way to feeling better, even if you're not sure you are on the right track. 
         <br>
 
         Overall, I want you to know that THIS IS A PROCESS. And you will never get it done, it will never be perfect, and that's ok.<br>
         We named it 'Feel better' rather than 'Be perfect' . My genuine wish for you is to enjoy this journey, and go as far as you want, at your own pace. 
         <br><br>
         I know this is not easy. I am proud of you for looking for help.<br> <br>
-        Another thing that this is not, it is not a professional trainer or eating program. I am fat positive and trans-inclusive, and I believe the health and happiness can be find in any size, and if anyone tells you otherwise, it is because they are unhappy with themselves. PROMISE. 
-        That's why all the dots are called: 'eat HEALTHIER' not ' healthy'. If you eat only salad all day and be miserable about it, this is not the outcome that you want, right? You finally got to eat home made meal instead of takeout? Great! Made something with vegetables you don't like? Fabolous! Eaten five Oreos instead of entire pack like yesterday? Congratulations, here is your dot!
+        Another thing that this is not, it is not a professional trainer or eating program. I believe the health and happiness can be find in any size, and if anyone tells you otherwise, it is because they are unhappy with themselves. PROMISE. 
+        That's why all the dots are called: 'eat HEALTHIER' not ' healthy'. If you finally got to eat home-made meal instead of takeout? Great! 
         
     </p>
 </section>
