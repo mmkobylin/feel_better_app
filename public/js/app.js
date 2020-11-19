7a7ee21d0b247a4cf23bbebe62194c5803266a32
mmkobylin <mmkobylin@gmail.com>
2020-11-19 09:41:17 +0000

    footer created

--- public/js/app.js
+++ public/js/app.js
@@ -19373,11 +19373,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-<<<<<<< HEAD
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nError: ENOENT: no such file or directory, open '/mnt/c/Users/mmkob/Videos/Code/feel-better-app/resources/sass/app.scss'\n    at /mnt/c/Users/mmkob/Videos/Code/feel-better-app/node_modules/webpack/lib/NormalModule.js:316:20\n    at /mnt/c/Users/mmkob/Videos/Code/feel-better-app/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /mnt/c/Users/mmkob/Videos/Code/feel-better-app/node_modules/loader-runner/lib/LoaderRunner.js:203:19\n    at /mnt/c/Users/mmkob/Videos/Code/feel-better-app/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:85:15\n    at processTicksAndRejections (internal/process/task_queues.js:79:11)");
-=======
 // removed by extract-text-webpack-plugin
->>>>>>> feature/style
 
 /***/ }),
 
