e08310e790a7a2fb88f3328b4a25825e9de34510
mmkobylin <mmkobylin@gmail.com>
2020-11-19 10:26:36 +0000

    committing before merging

--- public/js/app.js
+++ public/js/app.js
@@ -19366,30 +19366,26 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
-/***/ "./resources/sass/app.scss":
-/*!*********************************!*\
-  !*** ./resources/sass/app.scss ***!
-  \*********************************/
+/***/ "./resources/sass/app2.scss":
+/*!**********************************!*\
+  !*** ./resources/sass/app2.scss ***!
+  \**********************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-<<<<<<< HEAD
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nError: ENOENT: no such file or directory, open '/mnt/c/Users/mmkob/Videos/Code/feel-better-app/resources/sass/app.scss'\n    at /mnt/c/Users/mmkob/Videos/Code/feel-better-app/node_modules/webpack/lib/NormalModule.js:316:20\n    at /mnt/c/Users/mmkob/Videos/Code/feel-better-app/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /mnt/c/Users/mmkob/Videos/Code/feel-better-app/node_modules/loader-runner/lib/LoaderRunner.js:203:19\n    at /mnt/c/Users/mmkob/Videos/Code/feel-better-app/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:85:15\n    at processTicksAndRejections (internal/process/task_queues.js:79:11)");
-=======
-// removed by extract-text-webpack-plugin
->>>>>>> feature/style
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nError: ENOENT: no such file or directory, open '/mnt/c/Users/mmkob/Videos/Code/feel-better-app/resources/sass/app2.scss'\n    at /mnt/c/Users/mmkob/Videos/Code/feel-better-app/node_modules/webpack/lib/NormalModule.js:316:20\n    at /mnt/c/Users/mmkob/Videos/Code/feel-better-app/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /mnt/c/Users/mmkob/Videos/Code/feel-better-app/node_modules/loader-runner/lib/LoaderRunner.js:203:19\n    at /mnt/c/Users/mmkob/Videos/Code/feel-better-app/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:85:15\n    at processTicksAndRejections (internal/process/task_queues.js:79:11)");
 
 /***/ }),
 
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!**************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app2.scss ***!
+  \**************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /mnt/c/Users/mmkob/Videos/Code/feel-better-app/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /mnt/c/Users/mmkob/Videos/Code/feel-better-app/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /mnt/c/Users/mmkob/Videos/Code/feel-better-app/resources/sass/app2.scss */"./resources/sass/app2.scss");
 
 
 /***/ })
